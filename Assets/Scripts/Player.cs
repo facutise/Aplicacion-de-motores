de1da245ba89ef0b930ef09af1387f00b8b17824
Marco <marco.lavacchielli@davinci.edu.ar>
2023-08-06 21:02:55 -0300

    Getters y setters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,51 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    /*
-     public Vector3 move;
-     Rigidbody myRig;
-     [SerializeField] float speed=200;
+    private float _yVelocity;
+    [SerializeField] private float _speed = 200f;
 
-     private void Awake()
-     {
-         myRig = GetComponent<Rigidbody>();
-     }
-     float yfall;
-     private void FixedUpdate()
-     {
-         yfall=myRig.velocity.y;
-         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
 
-         myRig.velocity = move * Time.deltaTime*speed;
-         //control de caida
-         myRig.velocity = new Vector3(myRig.velocity.x,yfall,myRig.velocity.z);
+    public float yVelocity
+    {
+        get { return _yVelocity; }
+        set { _yVelocity = value; }
+    }
 
-
-     }
-     void Update()
-     {
-
-     }
-    */
-
-    //public float horizonalMove;
-    //public float verticalMove;
     private Vector3 playerInput;
     public Camera mainCamera;
     private Vector3 camForward;
     private Vector3 camRight;
     private Vector3 movePLayer;
-    //CombatPosition _combatPosition;
     Charview view;
     public MenuManager menumanagerscript;
     public float playerspeed;
     public int _maxhealth = 30; 
     public int vigorPoints = 40;
-    //public float gravity = 9.8f;
 
     public float _currenthealth; 
 
-    
 
     private void Awake()
     {
@@ -65,19 +48,9 @@ public class Player : MonoBehaviour
             menumanagerscript.Restartscene();
         }
 
-        /*horizonalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
-        //PlayerDies();
-        playerInput = new Vector3(horizonalMove, 0, verticalMove);
-        playerInput = Vector3.ClampMagnitude(playerInput, 1);*/
-
         camDirection();
 
         movePLayer = playerInput.x * camRight + playerInput.z * camForward;
-
-
-        //setGravity();
-
 
         if(movePLayer.magnitude > 0.3f)
         {
@@ -102,19 +75,5 @@ public class Player : MonoBehaviour
         camForward = camForward.normalized;
         camRight = camRight.normalized;
     }
-    /*void setGravity()
-    {
-        movePLayer.y = -gravity * Time.deltaTime;
-
-    }*/
-    /*public void PlayerDies()
-    {
-        if (PlayerHealth <= 0)
-        {
-            _combatPosition.salircombate();
-            Destroy(gameObject);
-            PlayerHealth = 30;
-        }
-    }*/
 
 }
